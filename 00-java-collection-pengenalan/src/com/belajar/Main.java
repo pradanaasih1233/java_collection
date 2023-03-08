d51b5e5221c9c916a531286fed48e193ad3ad849
PAPA <85855470+pradanaasih1233@users.noreply.github.com>
2023-03-08 23:30:44 +0700

    Add files via upload

---
+++ 00-java-collection-pengenalan/src/com/belajar/Main.java
@@ -0,0 +1,31 @@
+// JAVA COLLECTION
+
+/*------------------------------------------------------------
+*| JAVA COLLECTION
+*|------------------------------------------------------------
+*| - Collection atau sering di sebut container, adalah object
+*|   yang mengumpulkan atau berisikan data-data, seperti array.
+*| - Java telah menyediakan class collection yang langsung bisa
+*|   digunakan, tanpa menambah libary.
+*| - Collection di java adalah generic.
+*| - Java collection telah menyediakan segalanya, jadi tinggal
+*|   pakai.
+*|
+*|---DI-JAVA-TELAH-DISEDIAKAN-SEMUANYA-------------------------
+*|
+*| - Interfaces ini memiliki representasi dari collection.
+*| - Implementarions, bisa menggunakan implementasi yang sudah
+*|   dibat oleh java, semua interface collection sudah ada
+*|   implementasi classnya di java collection.
+*| - Algorithms, java juga sudah menyediakan algoritma yang umum
+*|   digunakan di collection, seperti pencarian dan pengurutan.
+*/
+
+package com.belajar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nBELAJAR JAVA 00 JAVA COLLECTION");
+
+    }
+}
